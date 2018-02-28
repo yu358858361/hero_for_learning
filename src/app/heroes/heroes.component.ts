f6d034dcb779f94c4dec386adc7676ad7dc74de4
于飞 <358858361@qq.com>
2018-02-28 19:30:33 +0800

    Angular官方hero实例的Routing完全完成后代码。

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,16 +11,10 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
