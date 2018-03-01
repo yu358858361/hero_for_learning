7e2d4ec3c6069b800b458fe20dbcd728da5d9470
于飞 <358858361@qq.com>
2018-03-01 15:27:50 +0800

    Revert "Angular官方hero实例的Routing完全完成后代码。"
    This reverts commit f6d034dcb779f94c4dec386adc7676ad7dc74de4.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-
-import { HeroService }  from '../hero.service';
+ 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -12,24 +9,9 @@ import { HeroService }  from '../hero.service';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
  
-  constructor(
-    private route: ActivatedRoute,
-    private heroService: HeroService,
-    private location: Location
-  ) {}
+  constructor() { }
  
-  ngOnInit(): void {
-    this.getHero();
-  }
-  
-  getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
-  }
-  
-  goBack(): void {
-    this.location.back();
+  ngOnInit() {
   }
  
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,16 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
+  selectedHero: Hero;
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   getHeroes(): void {
